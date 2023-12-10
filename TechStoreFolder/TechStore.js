c6e8335f49f0ef784f61b569badd147073e678af
ArtonLoshaj <al66192@ubt-uni.net>
2023-12-10 20:04:48 +0100

    java script commit
    Co-Authored-By: ArtLoshaj <149767735+ArtLoshaj@users.noreply.github.com>

--- TechStoreFolder/TechStore.js
+++ TechStoreFolder/TechStore.js
@@ -19,3 +19,58 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+let emailRegex= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+let passwordRegex= /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,}$/;
+
+
+function kyciratu() {
+    let emailError=document.getElementById('emailgabim');
+    let emailInput = document.getElementById('ID');
+    let passwordInput=document.getElementById('pass');
+    let passwordError = document.getElementById('passigabim');
+
+    emailError.innerText = '';
+    passwordError.innerText = '';
+
+    if(!emailRegex.test(emailInput.value)) {
+        emailError.innerText='Invalid email';
+      
+    }
+
+    if(passwordRegex.test(passwordInput.value)) {
+        passwordError.innerText='Invalid password';
+        return;
+    }
+
+    alert('Logging you in :)');
+}
+
+function registeratu() {
+    let emailError=document.getElementById('emailgabimr');
+    let emailInput = document.getElementById('email');
+    let passwordInput=document.getElementById('pass');
+    let passwordError = document.getElementById('passigabimr');
+    let emriError = document.getElementById('emrigabim');
+    let emriInput = document.getElementById('User');
+
+    emailError.innerText = '';
+    passwordError.innerText = '';
+    emriError.innerText = '';
+
+    if(!nameRegex.test(emriInput.value))
+    {
+        emriError.innerText='This username is not allowed';
+    }
+
+    if(!emailRegex.test(emailInput.value)) {
+        emailError.innerText='Invalid email';
+    }
+
+    if(!passwordRegex.test(passwordInput.value)) {
+        passwordError.innerText='Invalid password';
+        return;
+    }
+
+    alert('Your data has been saved');
+}
